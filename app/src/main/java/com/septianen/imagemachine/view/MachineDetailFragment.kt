5645fc16a8e78523ad3d9eafdfc8af4ee76a2333
septianen <developer.septianen@gmail.com>
2023-08-17 18:22:01 +0700

    Add function to hide keyboard after click done

--- app/src/main/java/com/septianen/imagemachine/view/MachineDetailFragment.kt
+++ app/src/main/java/com/septianen/imagemachine/view/MachineDetailFragment.kt
@@ -1,12 +1,14 @@
 package com.septianen.imagemachine.view
 
 import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
@@ -212,6 +214,8 @@ class MachineDetailFragment : Fragment(), MachineListener, DialogListener {
         binding.tilType.error = null
         binding.tilNumber.error = null
         binding.tilDate.error = null
+
+        hideKeyboard()
     }
 
     private fun updateData() {
@@ -324,5 +328,10 @@ class MachineDetailFragment : Fragment(), MachineListener, DialogListener {
             }
         }
         findNavController().popBackStack()
+    }
+
+    private fun hideKeyboard() {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
